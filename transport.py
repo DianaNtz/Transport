76195afc7bb736c0c3cab3d5a4799327e608e32d
Diana Nitzschke <diana.nitzschke@uni-jena.de>
2025-05-22 15:36:36 +0200

    added finite difference derivative

--- transport.py
+++ transport.py
@@ -16,3 +16,16 @@ nt=120*1
 t0=0
 tfinal=0.5
 dt=(tfinal-t0)/(nt-1)
+#finite difference derivative
+def first(D):
+    n=len(D)
+    Dr=np.zeros(n)
+    Dr[0]=(D[0+1]-D[0])/(h)
+    Dr[n-1]=(D[n-1]-D[n-1-1])/(h)
+    for i in range(0,n-2):
+        Dr[i+1]=(D[i+2]-D[i])/(2*h)
+    return Dr
+
+
+un=np.e**(-100*x**2/2)
+
